721430b43e73f1bc4f8379593a668bd840ff4415
YujiTakebuchi <takeyou.swift@gmail.com>
2023-03-18 23:38:04 +0900

    固定プレゼンテーションページのTSエラー解消

--- pages/gimmik/fixedPresentation.tsx
+++ pages/gimmik/fixedPresentation.tsx
@@ -2,6 +2,7 @@ import FixedSlide01 from "@/components/gimmik/fixedSlide/FixedSlide01";
 import FixOnAvailable from "@/components/gimmik/FixOnAvailable";
 import FixedPresentation from "@/components/gimmik/organisms/FixedPresentation";
 import { responsiveCSS } from "@/features/util";
+import { css } from "@emotion/react";
 
 export default function Home() {
   const slideStyleList: Array<string> = [
@@ -10,7 +11,10 @@ export default function Home() {
   ];
   return (
     <div style={{ padding: "2000px 0" }}>
-      <FixOnAvailable fixedStyleObjectList={slideStyleList}>
+      <FixOnAvailable
+        fixedStyleObjectList={slideStyleList}
+        cssOverrides={css``}
+      >
         <FixedPresentation
           slides={[
             <FixedSlide01 key={"s1"} fixedStyleObjectList={slideStyleList} />,
